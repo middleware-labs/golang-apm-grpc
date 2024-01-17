876e92093b512b369390363119385642842c6609
meealmathur23 <meenalmathur23@gmail.com>
2024-01-17 14:08:36 +0530

    update otel version

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -3,6 +3,7 @@ package grpc
 import (
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/stats"
 )
 
 func UnaryClientInterceptor() grpc.UnaryClientInterceptor {
@@ -19,4 +20,7 @@ func StreamServerInterceptor() grpc.StreamServerInterceptor {
 
 func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
 	return otelgrpc.UnaryServerInterceptor()
+}
+func NewClientHandler(opts otelgrpc.Option) stats.Handler {
+	return otelgrpc.NewClientHandler(opts)
 }
